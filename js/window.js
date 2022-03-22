eadff43c50520b165292c9175ac0dbdb31d4b7c1
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2022-03-22 23:23:03 +0900

    123

--- js/window.js
+++ js/window.js
@@ -1,8 +1,14 @@
 const userWindow=document.getElementById("window");//window는 이미 있으므로 변수명 userWdindow로 명명
 const closeButton=document.getElementById("window-top-button");
+const icon2=document.getElementById("icon2");
+
+
 
 function closeWindow(){
     userWindow.classList.add("hidden");
 }
 
 closeButton.addEventListener("click",closeWindow);
+icon2.addEventListener("dblclick",()=>{
+    userWindow.classList.remove("hidden");
+});
