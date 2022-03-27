4edce587e990ac39ccefc53d80cd772e50ce4a5f
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2022-03-27 20:40:27 +0900

    123

--- js/nextandback.js
+++ js/nextandback.js
@@ -14,63 +14,92 @@ const imgAndText=[
     },
     {
         image:"./img/logo.png",
-        header:"hello",
+        header:"hellonaver",
         text:"22222222222",
-        url:"#"
+        url:"http://naver.com"
     },
     {
         image:"./img/music.png",
-        header:"music",
+        header:"musicyoutube",
         text:"3333333333333333",
-        url:"#"
+        url:"http://youtube.com"
     }
 ]
+
+
 
 
 
 sectionImg.style.backgroundImage='url('+imgAndText[0].image+')';
 sectionheader.innerText=imgAndText[0].header;
 sectionText.innerText=imgAndText[0].text;
-
-
+navBack.disabled=true;
 let i=0;
-if(i==0){
-    navBack.classList.add("hidebutton");
-    navNext.classList.add("click");
-    navEnter.classList.add("click");
-}
 function clickNext(){
     i++;
-    navBack.classList.remove("hidebutton");
-    navBack.classList.add("click");
+    navNext.disabled=false;
+    navBack.disabled=false;
     sectionImg.style.backgroundImage='url('+imgAndText[i].image+')';
     sectionheader.innerText=imgAndText[i].header;
     sectionText.innerText=imgAndText[i].text;
+    navEnter.href=imgAndText[i].url;
     if(i==imgAndText.length-1){
-        navBack.classList.add("click");
-        navNext.classList.add("hidebutton");
-        navNext.classList.remove("click");
+        navNext.disabled=true;
     }
 }
-
-
-
-
 function clickBack(){
-    if(i==0){
-        navBack.classList.add("hidebutton");
-        navBack.classList.remove("click");
-        navNext.classList.add("click");
-        navBack.removeEventListener('click',clickBack);
-    }
     i--;
-    navNext.classList.remove("hidebutton");
-    navNext.classList.add("click");
+    navNext.disabled=false;
+    navBack.disabled=false;
     sectionImg.style.backgroundImage='url('+imgAndText[i].image+')';
     sectionheader.innerText=imgAndText[i].header;
     sectionText.innerText=imgAndText[i].text;
+    navEnter.href=imgAndText[i].url;
+    if(i==0){
+        navBack.disabled=true;
+    }
 }
-
-
 navNext.addEventListener('click',clickNext);
 navBack.addEventListener('click',clickBack);
+
+// let i=0;
+// if(i==0){
+//     navBack.classList.add("hidebutton");
+//     navNext.classList.add("click");
+//     navEnter.classList.add("click");
+// }
+// function clickNext(){
+//     i++;
+//     navBack.classList.remove("hidebutton");
+//     navBack.classList.add("click");
+//     sectionImg.style.backgroundImage='url('+imgAndText[i].image+')';
+//     sectionheader.innerText=imgAndText[i].header;
+//     sectionText.innerText=imgAndText[i].text;
+//     if(i==imgAndText.length-1){
+//         navBack.classList.add("click");
+//         navNext.classList.add("hidebutton");
+//         navNext.classList.remove("click");
+//     }
+// }
+
+
+
+
+// function clickBack(){
+//     if(i==0){
+//         navBack.classList.add("hidebutton");
+//         navBack.classList.remove("click");
+//         navNext.classList.add("click");
+//         navBack.removeEventListener('click',clickBack);
+//     }
+//     i--;
+//     navNext.classList.remove("hidebutton");
+//     navNext.classList.add("click");
+//     sectionImg.style.backgroundImage='url('+imgAndText[i].image+')';
+//     sectionheader.innerText=imgAndText[i].header;
+//     sectionText.innerText=imgAndText[i].text;
+// }
+
+
+// navNext.addEventListener('click',clickNext);
+// navBack.addEventListener('click',clickBack);
--- js/window.js
+++ js/window.js
@@ -1,16 +1,20 @@
 const userWindow=document.getElementById("window");//window는 이미 있으므로 변수명 userWdindow로 명명
 const closeButton=document.getElementById("window-top-button");
 const icon2=document.getElementById("icon2");
-
+const taskbarThings=document.getElementById('taskbar-things');
 
 
 function closeWindow(){
     userWindow.classList.add("hidden");
+    taskbarThings.classList.remove('view');
+    taskbarThings.innerText=null;
 }
 
 closeButton.addEventListener("click",closeWindow);
 icon2.addEventListener("dblclick",()=>{
     userWindow.classList.remove("hidden");
+    taskbarThings.classList.add('view');
+    taskbarThings.innerText='Helloo';
 });
 
 
