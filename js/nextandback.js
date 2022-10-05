411836e8b28b8def407feed51b6a5c47b9898c28
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2022-10-05 19:05:13 +0900

    123

--- js/nextandback.js
+++ js/nextandback.js
@@ -7,16 +7,16 @@ const navEnter=document.querySelector("#window-nav-enter");
 
 const imgAndText=[
     {
-        image:"./img/momentum.PNG",
+        image:"./img/momentum.jpg",
         header:"momentum",
-        text:"유튜버 노마드 코더님의 강의를 듣고 만든 구글 모멘텀 웹페이지 클론코딩입니다",
+        text:"유튜버 노마드 코더님의 강의를 듣고 만든 구글 모멘텀 웹페이지 클론코딩입니다\n 기능으로는 랜덤 배경화면,로그인,할 일 목록,현재 위치와 날씨조회가 있습니다.\n개발기간은 약 한달입니다.\n\n\n\n\n\n\n\n기술스택\nJavaScript,CSS,HTML",
         url:"https://kangaroo19.github.io/Momentum/momentem/index.html"
     },
     {
-        image:"./img/logo.png",
-        header:"hellonaver",
-        text:"22222222222",
-        url:"http://naver.com"
+        image:"./img/cardgame.gif",
+        header:"cardmatching",
+        text:"어릴 때 재미있게 플레이하던 '메이플스토리'라는 게임 안에 있는 미니게임인 같은그림찾기게임을 채팅기능을 포함해 1vs1 온라인으로 할 수 있게 웹 상에 구현하였습니다.\n개발기간은 약 한달 반 정도입니다.\n\n\n\n\n\n\n\n기술스택\nJavaScript,NodeJS,CSS,HTML",
+        url:"https://chatting22.herokuapp.com/"
     },
     {
         image:"./img/music.png",
