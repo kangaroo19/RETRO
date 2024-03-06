52ca4a9e5a5675570d9d9ce6c7e18e8767d62f9c
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2024-03-06 16:43:46 +0900

    이미지추가,스타일 수정

--- js/window.js
+++ js/window.js
@@ -1,89 +1,82 @@
-const userWindow=document.getElementById("window");//window는 이미 있으므로 변수명 userWdindow로 명명
-const myInfo=document.getElementById('my-info')
-const closeButton=document.getElementById("close-button");
-const icon1=document.getElementById("icon1");
-const icon2=document.getElementById("icon2");
-const taskbarThings=document.getElementById('taskbar-things');
-const errorBtn=$('#error-window-bottom').children()
+const userWindow = document.getElementById("window"); //window는 이미 있으므로 변수명 userWdindow로 명명
+const myInfo = document.getElementById("my-info");
+const closeButton = document.getElementById("close-button");
+const icon1 = document.getElementById("icon1");
+const icon2 = document.getElementById("icon2");
+const taskbarThings = document.getElementById("taskbar-things");
+const errorBtn = $("#error-window-bottom").children();
 
 taskbarThings.classList.remove("view");
 
-function closeWindow(){
-    userWindow.classList.add("hidden");
-    taskbarThings.classList.remove('view');
-    taskbarThings.innerText=null;
-    
+function closeWindow() {
+  userWindow.classList.add("hidden");
+  taskbarThings.classList.remove("view");
+  taskbarThings.innerText = null;
 }
 
-closeButton.addEventListener("click",closeWindow);
+closeButton.addEventListener("click", closeWindow);
 // icon2.addEventListener("dblclick",(event)=>{
 //     userWindow.classList.remove("hidden");
-    
-  
+
 // });
-$('#nav-button5').on('click',()=>{
-    $('#my-info').addClass('hidden')
-    taskbarThings.classList.remove('view')
-    taskbarThings.innerText=null;
+$("#nav-button5").on("click", () => {
+  $("#my-info").addClass("hidden");
+  taskbarThings.classList.remove("view");
+  taskbarThings.innerText = null;
+});
+$("#close-button2").click(() => {
+  console.log(123232323);
+  $("#my-info").addClass("hidden");
+  taskbarThings.classList.remove("view");
+  taskbarThings.innerText = null;
+});
+taskbarThings.classList.add("view");
+taskbarThings.innerText = "My Information";
+icon1.addEventListener("dblclick", () => {
+  if (userWindow.className === "animate__animated animate__fadeIn ui-draggable") {
+    return;
+  } else {
+    $("#my-info").removeClass("hidden");
 
-})
-$('#close-button2').click(()=>{
-    console.log(123232323)
-    $('#my-info').addClass('hidden')
-    taskbarThings.classList.remove('view')
-    taskbarThings.innerText=null;
+    taskbarThings.classList.add("view");
+    taskbarThings.innerText = "My Information";
+  }
+});
+$(".icon-img-text").draggable();
 
-})
-taskbarThings.classList.add('view');
-taskbarThings.innerText='My Information';
-icon1.addEventListener('dblclick',()=>{
-    if(userWindow.className==='animate__animated animate__fadeIn ui-draggable'){
-        return
-    }
-    else{
-        $('#my-info').removeClass('hidden') 
-        
-        taskbarThings.classList.add('view');
-        taskbarThings.innerText='My Information';
-    }
-})
-$('.icon-img-text').draggable()
+$("#window").draggable();
+$("#my-info").draggable();
 
-$('#window').draggable()
- $('#my-info').draggable()
+icon2.addEventListener("dblclick", () => {
+  console.log(myInfo.className);
+  if (myInfo.className == "animate__animated animate__fadeIn ui-draggable") {
+    return;
+  } else {
+    $("#window").removeClass("hidden");
+    taskbarThings.classList.add("view");
+    taskbarThings.innerText = "My projects";
+  }
+});
+$("#icon3").on("dblclick", () => {
+  $("#error-window").removeClass("hidden");
+});
 
-icon2.addEventListener('dblclick',()=>{
-    console.log(myInfo.className)
-    if(myInfo.className=='animate__animated animate__fadeIn ui-draggable'){
-        return  
-    }
-    else{
-        $('#window').removeClass('hidden')
-        taskbarThings.classList.add('view');
-        taskbarThings.innerText='My projects';
-    }
-})
-$('#icon3').on('dblclick',()=>{
-    $('#error-window').removeClass('hidden')
-})
+$(errorBtn).on("click", () => {
+  $("#error-window").addClass("hidden");
+});
+const scroll2 = document.getElementById("my-info-article-section");
+$("#nav-button1").click(() => {
+  // audio.play()
+  console.log(scroll2.scrollTop); //현재좌표 찍는거
+  console.log(scroll2.scrollTo(0, 0));
+});
+$("#nav-button2").click(() => {
+  scroll2.scrollTo(0, 300); //움직이는거
+});
 
-
-$(errorBtn).on('click',()=>{
-    $('#error-window').addClass('hidden')
-})
-const scroll2=document.getElementById('my-info-article-section')
-$('#nav-button1').click(()=>{
-    // audio.play()
-    console.log(scroll2.scrollTop) //현재좌표 찍는거
-    console.log(scroll2.scrollTo(0,0))
-})
-$('#nav-button2').click(()=>{
-    scroll2.scrollTo(0,300) //움직이는거
-})
-
-$('#nav-button3').click(()=>{
-    scroll2.scrollTo(0,500)
-})
-$('#nav-button5').click(()=>{
-    $('#my-info').addClass('hidden')
-})
+$("#nav-button3").click(() => {
+  // scroll2.scrollTo(0,500)
+});
+$("#nav-button5").click(() => {
+  $("#my-info").addClass("hidden");
+});
